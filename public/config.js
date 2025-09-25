bb22855f70a2e82194c606d314ee57c3b34f1915
Mario Reder <mario.reder@pm.me>
2025-09-25 16:39:11 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
-  "VITE_CUSTOM_MENUS": "Docs, https://docs.yellowperps.com; Portal, https://portal.yellowperps.com",
+  "VITE_CUSTOM_MENUS": "Docs, https://docs.yellowperps.com",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
